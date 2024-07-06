47bd3452ae1240a636817d6cba62f14ea5e4be63
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-07-06 10:48:35 +0200

    feat: request for store lead created and store tested with postman

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreLeadRequest;
+use App\Models\Lead;
 use Illuminate\Http\Request;
 
 class LeadController extends Controller
 {
-    public function store(Request $request) {
+    public function store(StoreLeadRequest $request)
+    {
+        $data = $request->validated();
+        $newLead = new Lead();
+        $newLead->fill($data);
+        $newLead->save();
         return response()->json([
             'results' => 'ok'
         ]);
