4263e8533688c89d3209fe864609121e7a2458a2
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-07-06 10:26:34 +0200

    feat: route, controller and model create for mail leads

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('projects/{slug}', [ProjectController::class, 'show']);
 
+Route::post('/leads', [LeadController::class, 'store']);
